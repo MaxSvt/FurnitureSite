902bae92820437e28fbd72ea6bda87e015c7ed9e
MaxSvt <51627564+MaxSvt@users.noreply.github.com>
2022-06-04 15:49:09 +0300

    Create settings.py

--- Furniture/furniture_site/furniture_site/settings.py
+++ Furniture/furniture_site/furniture_site/settings.py
@@ -98,8 +98,8 @@ DATABASES = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gastings1999@gmail.com'
-EMAIL_HOST_PASSWORD = 'Sunskritprod2999*'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
